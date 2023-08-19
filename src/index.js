19d6beebe3349047227043e92a225382fb55b178
Cassius Reynolds <cassius2828@gmail.com>
2023-08-19 15:57:43 -0700

    downgraded to react 17.0.2 and now all tests pass

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// react 17.0.2
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-
